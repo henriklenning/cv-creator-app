823f57928ea30044d3b14f292667d59072a5ef69
henriklenning <henrik.fohrenbachlenning@gmail.com>
2023-12-07 11:48:40 +0100

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,6 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <h1>CV Creator App</h1>
-    </>
-  )
 }
 
 export default App
