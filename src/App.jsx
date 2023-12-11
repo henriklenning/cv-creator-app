55ef54af439026f45ddf9103fa4202cd2a3dc25e
henriklenning <henrik.fohrenbachlenning@gmail.com>
2023-12-11 15:04:41 +0100

    play around with list component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import Message from "./Message";
-
+import ListGroup from "./components/ListGroup";
 function App() {
-    return <div><Message /></div>;
+    return <div><ListGroup /></div>;
 }
 
 export default App
